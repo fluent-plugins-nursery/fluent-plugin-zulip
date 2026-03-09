5aaf0fc4a4b6b5ed7a787bd7e98a1101cd9943c5
Shizuo Fujita <fujita@clear-code.com>
2026-03-09 15:18:52 +0900

    Remove user agent assertion

--- test/plugin/test_out_zulip.rb
+++ test/plugin/test_out_zulip.rb
@@ -73,7 +73,6 @@ class ZulipOutputTest < Test::Unit::TestCase
       headers = {
         "Authorization" => "Basic ZXhhbXBsZS1ib3RAZXhhbXBsZS5jb206dmVyeS1zZWNyZXQtdmFsdWU=",
         "Expect" => "",
-        "User-Agent" => "Faraday v1.10.5",
       }
       stub_request(:post, "https://zulip.example.com/api/v1/messages")
         .with(basic_auth: ["example-bot@example.com", "very-secret-value"],
@@ -97,7 +96,6 @@ class ZulipOutputTest < Test::Unit::TestCase
       headers = {
         "Authorization" => "Basic ZXhhbXBsZS1ib3RAZXhhbXBsZS5jb206dmVyeS1zZWNyZXQtdmFsdWU=",
         "Expect" => "",
-        "User-Agent" => "Faraday v1.10.5",
       }
       stub_request(:post, "https://zulip.example.com/api/v1/messages")
         .with(basic_auth: ["example-bot@example.com", "very-secret-value"],
